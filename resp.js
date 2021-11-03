25dff957a8c4237ec06fc3d19c996bf2e4885de8
safwantaliparamba <safwansafu242090@gmail.com>
2021-11-03 18:07:29 +0530

    second commit

--- resp.js
+++ resp.js
@@ -5,3 +5,11 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }   
+
+// let formBtn = document.querySelector('.form-btn')
+
+//   formBtn.addEventListener('click' , ()=>{
+//     const name = document.querySelector('.f-name').value
+//     console.log(name)
+
+//   })
